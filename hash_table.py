d7c09bf66378e573083e9fd6d91a4fe1ee3b9420
Danielle Puga <danielle.puga@gmail.com>
2019-10-13 13:47:30 -0700

    passed 14 tests

--- hash_table.py
+++ hash_table.py
@@ -8,19 +8,32 @@ class HashTable:
 
     def __init__(self, size=10):
         self.size = size
-        self.data = []
-        self.keys = []
-        # self.hash = hash()
-        
-    def __setitem__(self, key, value):
-        self.data.append(value)
-        self.keys.append(key)
-
-    def __getitem__(self, key):
-        for k in self.keys: #looping through all keys
-            if k == key: #if k equals key we are looking for, the key sent into the method
-                return self.data[k]
-        return None
+        self.data = [ [] for i in range(self.size)]     
 
     def hash(self, key):
-        return hash(key)
+        size = (self.size)
+        return hash(key) % size
+        
+    def __setitem__(self, key, value):
+        key_hash = self.hash(key)
+        key_value = [key, value]
+
+        if self.data[key_hash] is not None:
+            for k in self.data[key_hash]:
+                if k[0] == key:
+                    k[1] = value
+                    return True
+            self.data[key_hash].append(key_value)
+        else:
+            self.data[key_hash].append([key, value])
+
+    def __getitem__(self, key):
+        index = self.hash(key)
+        for k in self.data[index]: #looping through all keys
+            if k[0] == key: #if k equals key we are looking for, the key sent into the method
+                return k[1]
+
+    # def delete(self, key, value):
+
+    def clear(self):
+        
